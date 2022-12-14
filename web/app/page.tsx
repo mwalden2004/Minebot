1c245379d5a794a55ddd239521fe98d4534af0e5
Matthew Walden <sloss196@gmail.com>
2022-12-14 14:32:31 -0600

    much better

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -4,12 +4,57 @@ import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
-const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+import {
+  BoltIcon,
+  ChatBubbleBottomCenterTextIcon,
+  EnvelopeIcon,
+  GlobeAltIcon,
+  ScaleIcon,
+} from '@heroicons/react/24/outline'
+
+const transferFeatures = [
+  {
+    id: 1,
+    name: 'Powerful Automations',
+    description:
+      'MineBot is built for extensabillity, and has many automations to make your life as a community-manager easy.',
+    icon: GlobeAltIcon,
+  },
+  {
+    id: 2,
+    name: 'Built by industry leaders',
+    description:
+      'Minebot is built by a team of experienced develoeprs who have been working on services and tools for years.',
+    icon: ScaleIcon,
+  },
+  {
+    id: 3,
+    name: 'Feature-Rich',
+    description:
+      'MineBot still in its infancy has many features and out-shines the commpetition. We still have a long plan ahead to help powerup not only your Discord, but your servers as well.',
+    icon: BoltIcon,
+  },
 ]
+const communicationFeatures = [
+  {
+    id: 1,
+    name: 'Constantly Developed',
+    description:
+      'MineBot is constantly adding new features, and fixing bugs.',
+    icon: ChatBubbleBottomCenterTextIcon,
+  },
+  {
+    id: 2,
+    name: '100% Uptime',
+    description:
+      'Since our release we have been able to boast a 100% up-time.',
+    icon: EnvelopeIcon,
+  },
+]
+
+const navigation = [
+
+] as {name: string; href: string;}[]
 
 export default function Home() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
@@ -71,10 +116,10 @@ export default function Home() {
             </div>
             <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
               <a
-                href="#"
+                href="https://discord.com/api/oauth2/authorize?client_id=1051598158850961468&permissions=8&scope=bot%20applications.commands"
                 className="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20"
               >
-                Log in
+                Invite Bot
               </a>
             </div>
           </nav>
@@ -117,10 +162,10 @@ export default function Home() {
                   </div>
                   <div className="py-6">
                     <a
-                      href="#"
+                      href="https://discord.com/api/oauth2/authorize?client_id=1051598158850961468&permissions=8&scope=bot%20applications.commands"
                       className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
                     >
-                      Log in
+                      Invite Bot
                     </a>
                   </div>
                 </div>
@@ -194,6 +239,174 @@ export default function Home() {
             </div>
           </div>
         </div>
+
+        <div className="overflow-hidden bg-gray-50 py-16 lg:py-24">
+      <div className="relative mx-auto max-w-xl px-6 lg:max-w-7xl lg:px-8">
+        <svg
+          className="absolute left-full hidden -translate-x-1/2 -translate-y-1/4 transform lg:block"
+          width={404}
+          height={784}
+          fill="none"
+          viewBox="0 0 404 784"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="b1e6e422-73f8-40a6-b5d9-c8586e37e0e7"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width={404} height={784} fill="url(#b1e6e422-73f8-40a6-b5d9-c8586e37e0e7)" />
+        </svg>
+
+        <div className="relative">
+          <h2 className="text-center text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+            Power up your Minecraft community
+          </h2>
+          <p className="mx-auto mt-4 max-w-3xl text-center text-xl text-gray-500">
+            Minebot is the leading Minecraft verification bot, built to scale with your needs.
+          </p>
+        </div>
+
+        <div className="relative mt-12 lg:mt-24 lg:grid lg:grid-cols-2 lg:items-center lg:gap-8">
+          <div className="relative">
+            <h3 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">See how it will help you</h3>
+            <p className="mt-3 text-lg text-gray-500">
+              With its ever-growning utillities, and growing list of feature plans it is the obvious choice for running your community.
+            </p>
+
+            <dl className="mt-10 space-y-10">
+              {transferFeatures.map((item) => (
+                <div key={item.id} className="relative">
+                  <dt>
+                    <div className="absolute flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-500 text-white">
+                      <item.icon className="h-8 w-8" aria-hidden="true" />
+                    </div>
+                    <p className="ml-16 text-lg font-medium leading-6 text-gray-900">{item.name}</p>
+                  </dt>
+                  <dd className="mt-2 ml-16 text-base text-gray-500">{item.description}</dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+
+          <div className="relative -mx-4 mt-10 lg:mt-0" aria-hidden="true">
+            <svg
+              className="absolute left-1/2 -translate-x-1/2 translate-y-16 transform lg:hidden"
+              width={784}
+              height={404}
+              fill="none"
+              viewBox="0 0 784 404"
+            >
+              <defs>
+                <pattern
+                  id="ca9667ae-9f92-4be7-abcb-9e3d727f2941"
+                  x={0}
+                  y={0}
+                  width={20}
+                  height={20}
+                  patternUnits="userSpaceOnUse"
+                >
+                  <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                </pattern>
+              </defs>
+              <rect width={784} height={404} fill="url(#ca9667ae-9f92-4be7-abcb-9e3d727f2941)" />
+            </svg>
+            <img
+              className="relative mx-auto rounded-lg"
+              width={490}
+              src="/verifying.png"
+              alt=""
+            />
+          </div>
+        </div>
+
+        <svg
+          className="absolute right-full hidden translate-x-1/2 translate-y-12 transform lg:block"
+          width={404}
+          height={784}
+          fill="none"
+          viewBox="0 0 404 784"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="64e643ad-2176-4f86-b3d7-f2c5da3b6a6d"
+              x={0}
+              y={0}
+              width={20}
+              height={20}
+              patternUnits="userSpaceOnUse"
+            >
+              <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width={404} height={784} fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
+        </svg>
+
+        <div className="relative mt-12 sm:mt-16 lg:mt-24">
+          <div className="lg:grid lg:grid-flow-row-dense lg:grid-cols-2 lg:items-center lg:gap-8">
+            <div className="lg:col-start-2">
+              <h3 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">It&apos;s reliable</h3>
+              <p className="mt-3 text-lg text-gray-500">
+                Always online, and always growing -- whats more to love?
+              </p>
+
+              <dl className="mt-10 space-y-10">
+                {communicationFeatures.map((item) => (
+                  <div key={item.id} className="relative">
+                    <dt>
+                      <div className="absolute flex h-12 w-12 items-center justify-center rounded-xl bg-indigo-500 text-white">
+                        <item.icon className="h-8 w-8" aria-hidden="true" />
+                      </div>
+                      <p className="ml-16 text-lg font-medium leading-6 text-gray-900">{item.name}</p>
+                    </dt>
+                    <dd className="mt-2 ml-16 text-base text-gray-500">{item.description}</dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+
+            <div className="relative -mx-4 mt-10 lg:col-start-1 lg:mt-0">
+              <svg
+                className="absolute left-1/2 -translate-x-1/2 translate-y-16 transform lg:hidden"
+                width={784}
+                height={404}
+                fill="none"
+                viewBox="0 0 784 404"
+                aria-hidden="true"
+              >
+                <defs>
+                  <pattern
+                    id="e80155a9-dfde-425a-b5ea-1f6fadd20131"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                  </pattern>
+                </defs>
+                <rect width={784} height={404} fill="url(#e80155a9-dfde-425a-b5ea-1f6fadd20131)" />
+              </svg>
+              <img
+                className="relative mx-auto rounded-lg"
+                width={490}
+                src="/lookup.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
       </main>
     </div>
   )
