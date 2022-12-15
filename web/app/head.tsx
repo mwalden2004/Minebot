e1d56e5279bf2d3866377bb1c89c5a5129526c43
Matthew Walden <sloss196@gmail.com>
2022-12-14 22:58:34 -0600

    Add tracking with Google analytics

--- web/app/head.tsx
+++ web/app/head.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export default function Head() {
   return (
     <>
@@ -5,6 +7,22 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Minebot is the leading Minecraft verification service." />
       <link rel="icon" href="/Black Logo.png" />
+
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-GLNMX6P0WD" />
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+     window.dataLayer = window.dataLayer || [];
+     function gtag(){dataLayer.push(arguments);}
+     gtag('js', new Date());
+     gtag('config', 'G-GLNMX6P0WD', {
+      page_path: window.location.pathname,
+     });
+    `,
+        }}
+      />
     </>
   )
 }
