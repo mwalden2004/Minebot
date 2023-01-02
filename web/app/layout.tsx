db8de37781b424cbb9eeac85e0e6217250d545d2
Matthew Walden <sloss196@gmail.com>
2023-01-02 13:23:43 -0600

    SEO

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,3 +1,4 @@
+import { NextSeo } from 'next-seo'
 import './globals.css'
 
 export default function RootLayout({
@@ -7,7 +8,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head />
+      <head>
+    <NextSeo
+      title="Unleash the Power of Your Minecraft Community with MineBot: The Leading Discord Verification Bot Free"
+      description="Welcome to MineBot, the leading Minecraft verification bot for Discord. With powerful automations and a growing list of features, MineBot is here to power up your Minecraft community. Invite MineBot to your Discord server today and join our community to see all that MineBot has to offer. Connect your Minecraft account with ease and unlock the full potential of your server with MineBot."
+    />
+    <meta name="keywords" content="minecraft,discord,minecraft server,server,bot,minecraft bot,discord bot,minecraft verification,verification,verify,automation,server automation,minecraft automation,community,minecraft community,discord community,discord server,server management,minecraft server magement,management,tools,minecraft tools,discord tools,minecraft resources,resources,discord resources,integration,utility,software,website,free,mine,bot,minebot"></meta>
+      </head>
       <body>
         {/*<Header />*/}
         {children}
